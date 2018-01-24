31aff1d24b5bf97d2bfe51908994a5a0a6775f1c
sky <hyunwoo126@gmail.com>
2018-01-24 18:18:25 -0500

    sw back up

--- sw.js
+++ sw.js
@@ -1,26 +1,33 @@
 //may need to add polyfill for cache
 
 self.addEventListener('install', function(e){
-    console.log('service worker install event');
+    console.log('INSTALL event');
     e.waitUntil(
       caches.open('offline-clock').then(function(cache){
         return cache.addAll([
           '/',
           '/index.html',
+          '/index.css',
+          '/moment.js',
         ]);
       })
     );
 });
 
+self.addEventListener('activate', function(event) {
+    console.log('Finally active. Ready to start serving content!');  
+});
+
+
 //intercept all request and return cached version, if exists
 self.addEventListener('fetch', function(event) {
+    console.log('online: ',navigator.onLine);
     console.log(event.request.url);
 
-    console.log('online status ', navigator.onLine);
-    
     event.respondWith(
         caches.match(event.request).then(function(response) {
             return response || fetch(event.request);
         })
     );
 });
+
